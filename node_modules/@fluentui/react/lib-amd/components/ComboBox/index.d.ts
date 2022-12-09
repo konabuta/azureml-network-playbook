e1cfebef81877575fffbaf219589b85977affe6c
Keita Onabuta <keonabut@microsoft.com>
2022-12-09 18:20:56 +0000

    node_modules

--- node_modules/@fluentui/react/lib-amd/components/ComboBox/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './ComboBox';
-export { IComboBoxClassNames, IComboBoxOptionClassNames } from './ComboBox.classNames';
-export * from './ComboBox.types';
-export * from './VirtualizedComboBox';
