9506d217ac7e77620edec61f7902186dd66c13f1
Keita Onabuta <keonabut@microsoft.com>
2022-12-12 03:45:57 +0900

    add surveyData and contentData

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -4,9 +4,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import surveyData from '../data/test/survey.json';
+import contentData from '../data/test/content.json';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App surveyData={surveyData} contentData={contentData}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
