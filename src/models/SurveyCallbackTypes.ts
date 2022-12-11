a6eadab8a6f6eb6eb36a1ebfed165a1eed605a54
Keita Onabuta <keonabut@microsoft.com>
2022-12-12 03:46:23 +0900

    upgrade surveyjs v1 to v2

--- src/models/SurveyCallbackTypes.ts
+++ src/models/SurveyCallbackTypes.ts
@@ -5,25 +5,25 @@
 // These types were not provided in the survey.js type definitions,
 // but they are documented in the official docs.
 
-import { PageModel, QuestionRadiogroupModel } from "survey-react";
+import { PageModel, QuestionRadiogroupModel } from "survey-core";
 
 export interface SurveyValueChangedOptions {
-  name: string,
-  question: QuestionRadiogroupModel,
-  value: string
+  name: string;
+  question: QuestionRadiogroupModel;
+  value: string;
 }
 
 export interface SurveyCompleteOptions {
-  showDataSaving: (text: string) => any,
-  showDataSavingError: (text: string) => any,
-  showDataSavingSuccess: (text: string) => any,
-  showDataSavingClear: () => any,
-  isCompleteOnTrigger: () => boolean
+  showDataSaving: (text: string) => any;
+  showDataSavingError: (text: string) => any;
+  showDataSavingSuccess: (text: string) => any;
+  showDataSavingClear: () => any;
+  isCompleteOnTrigger: () => boolean;
 }
 
 export interface CurrentPageChangedOptions {
-  oldCurrentPage: PageModel,
-  newCurrentPage: PageModel,
-  isNextPage: boolean,
-  isPrevPage: boolean
+  oldCurrentPage: PageModel;
+  newCurrentPage: PageModel;
+  isNextPage: boolean;
+  isPrevPage: boolean;
 }
